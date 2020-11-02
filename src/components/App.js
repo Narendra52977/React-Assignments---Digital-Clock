bf9fb226ecb475984c414e9f97ef672643556320
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-11-02 10:25:13 +0530

    edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import { Clock } from "./Clock";
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={clock:<Clock/>}
+    }
+    componentDidMount(){
+       this.inteval=setInterval(()=>{
+        this.setState({clock:<Clock/>})
+       },1000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.inteval);
+    }
     render() {
 
         return(
-            <>
-               
-            </>
+            <div className="Clock">
+               <h3 id="time">{this.state.clock}</h3>
+            </div>
         )
     }
 }
---
+++ src/components/Clock.js
@@ -0,0 +1,3 @@
+export function Clock() {
+  return new Date().toLocaleTimeString();
+}
